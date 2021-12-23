0e0d0fb575af9c5c142969876e5531e6856f842a
jennyshih <jinghua.shih@gmail.com>
2021-12-23 11:37:52 +0800

    Add an alternative approach for day 21

--- day21.rb
+++ day21.rb
@@ -3,7 +3,23 @@
 require './runner'
 require 'set'
 
-class QuantumDice
+# ============================================================
+# Notes:
+#
+# There are two versions for part 2.
+#
+# QuantumDiceGame is the efficient version. It takes 0.3 seconds to run but is more complex in its reasoning.
+# More math involved. Usage:
+#
+#     QuantumDiceGame.play(p1_init, p2_init)
+#
+# QuantumDiceGameAlternative is the more concise version but has higher time complexity (takes about ~10s to run).
+# It runs through every possible branch and add up the result. Usage:
+#
+#     QuantumDiceGameAlternative.play(p1_init, p2_init)
+# ============================================================
+
+class QuantumDiceGame
   DICE_POSSIBILITY = {
     3 => 1,
     4 => 3,
@@ -14,15 +30,28 @@ class QuantumDice
     9 => 1
   }.freeze
 
-  def initialize(init_pos)
-    @init_pos = init_pos
+  def self.play(p1_init, p2_init)
+    p1_complete, p1_incomplete = new.roll(p1_init)
+    p2_complete, p2_incomplete = new.roll(p2_init)
+
+    p1_winning_universes = p1_complete.sum do |step_count, universes|
+      p2_incomplete[step_count - 1] * universes
+    end
+    p2_winning_universes = p2_complete.sum do |step_count, universes|
+      p1_incomplete[step_count] * universes
+    end
+
+    [p1_winning_universes, p2_winning_universes].max
+  end
+
+  def initialize
     @incomplete_seq = Set.new
     @completed_seq = Set.new
   end
 
   # Roll to the end of the multiple universes!
-  def roll
-    generate_sequences(@init_pos, 0, [])
+  def roll(init_pos)
+    generate_sequences(init_pos, 0, [])
     generate_possibility_tables
   end
 
@@ -64,27 +93,35 @@ class QuantumDice
   end
 end
 
+# Alternative solution. Higher time complexity but much simpler.
+class QuantumDiceGameAlternative
+  def self.play(p1_init, p2_init)
+    dice = [1, 2, 3].flat_map { [_1] * 3 }.permutation(3).uniq.map(&:sum).tally
+    roll(p1_init, 0, p2_init, 0, dice).max
+  end
+
+  def self.roll(p1, p1_score, p2, p2_score, dice)
+    return [0, 1] if p2_score >= 21
+
+    dice.each_with_object([0, 0]) do |value_count, memo|
+      value, count = value_count
+      next_pos = move(p1, value)
+      p1_result, p2_result = roll(p2, p2_score, next_pos, p1_score + next_pos, dice)
+      memo[1] += p1_result * count
+      memo[0] += p2_result * count
+    end
+  end
+end
+
 class Day21 < Runner
   def do_puzzle2
-    p1_complete, p1_incomplete = QuantumDice.new(6).roll
-    p2_complete, p2_incomplete = QuantumDice.new(4).roll
-
-    p1_winning_universes = p1_complete.sum do |step_count, universes|
-      p2_incomplete[step_count - 1] * universes
-    end
-    p2_winning_universes = p2_complete.sum do |step_count, universes|
-      p1_incomplete[step_count] * universes
-    end
-
-    [p1_winning_universes, p2_winning_universes].max
+    QuantumDiceGame.play(4, 8)
   end
 
   def do_puzzle1
     p1 = 6
     p2 = 4
-    p1_score = 0
-    p2_score = 0
-    die_count = 0
+    p1_score = p2_score = die_count = 0
     die = 1
 
     loop do
